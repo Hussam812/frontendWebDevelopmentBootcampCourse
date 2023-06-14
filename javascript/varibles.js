f00d7ef8edcd319e457f238e649c62434c6d1ac5
Hussam812 <hossamhosnihussien@gmail.com>
2023-06-14 14:03:26 +0200

    varible

---
+++ javascript/varibles.js
@@ -0,0 +1,21 @@
+const myArray = [10, 20, 30, 40];
+myArray.push(50);
+console.log(myArray);
+console.log(myArray.reverse());
+
+const yourFirstVarible = "Learning to code gives you superpowers";
+const yourSecondVarible = 10;
+const yourThirdVarible = {firsitProperty : "Hello World"};
+
+console.log(yourFirstVarible);
+
+const newVArible ="hello world";
+let anotherVarible ;
+anotherVarible = 20;
+console.log(anotherVarible);
+
+
+const varible1 = 10;
+let varible2 = 10;
+var varible3 = 10;
+
